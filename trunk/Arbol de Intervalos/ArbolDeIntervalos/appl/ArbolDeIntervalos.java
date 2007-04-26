b363658430b73bb85923b91fb85224aab92f597f
fernando.bugni <fernando.bugni@284c10f3-ef2d-0410-b571-df4d41f224a4>
2007-04-26 02:37:00 +0000

    Acá esta la funcion InsertarIntervalo

--- trunk/Arbol de Intervalos/ArbolDeIntervalos/appl/ArbolDeIntervalos.java
+++ trunk/Arbol de Intervalos/ArbolDeIntervalos/appl/ArbolDeIntervalos.java
@@ -8,7 +8,7 @@ public class ArbolDeIntervalos {
 	/**
 	 * Este es el ancho del arbol de intervalos
 	 */
-	//private final int ANCHO_ARBOL= Parser.ANCHO;
+	private final int ANCHO_ARBOL= Parser.ANCHO;
 		
 	public ArbolDeIntervalos()
 	{
@@ -32,24 +32,37 @@ public class ArbolDeIntervalos {
 	}
 	
 	public void insertarImagen(int indexOf, Imagen img)
-	{/*
-		Nodo <IntervaloElemental> actual = this.getRaiz();
-		int min = 0;
-		int max = ANCHO_ARBOL;
-		
-		while( actual != null)
-		{
-			if((min <= actual.dato.numero) && (max <= actual.dato.numero)) 
-			{
-				actual.dato.indexOfImg.add(indexOf);
-				actual = null;
-			}else{
-				if()
-				{
-					//hasta aqui llegamos!!
-				}
-			}
-		}
-	 */
+	{
+		//tenemos en img las coordenadas que vamos a meter en la imagen
+		//Nombre de intervalo: indexOf
+		insertarIntervalo(indexOf, img.x_0, img.x_1, 0, ANCHO_ARBOL, this.getRaiz());
 	}
+	
+	@SuppressWarnings("unchecked")
+	public void insertarIntervalo(int indexOf, int a, int b, int min, int max, Nodo <IntervaloElemental> actual)
+	{
+		//el intervalo que quiero insertar es justo el donde estoy
+		if (( a <= min) && ( max<= b))
+		{
+			//guardo la imagen 
+			// aca falta la lista
+			actual.dato.numero = indexOf;
+		}else{
+			//sino quiere decir que debo bajar por una de sus hojas
+			//decido si bajo por una y/o por otra
+				
+			if(a < actual.dato.numero)
+			{
+				insertarIntervalo(indexOf, a, b, min, actual.dato.numero, actual.izq);
+			}
+
+			if(actual.dato.numero < b)
+			{
+				insertarIntervalo(indexOf, a, b, actual.dato.numero, max, actual.der);
+			}
+			
+		}
+		
+	}
+
 }
